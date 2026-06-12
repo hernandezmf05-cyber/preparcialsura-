69c252dcba6412aef901f7ce9b4bb8c15acc65c1
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2026-06-11 20:30:38 -0500

    feat se crea proyecto con ejemplo de variables

---
+++ algoritmoUno.py
@@ -0,0 +1,17 @@
+#Entradas del sistema
+nombre_usuario=input("digita tu nombre: ") 
+correo_usuario=input("digita tu correo: ") 
+contraseña_usuario=input("digita tu contraseña: ") 
+ciudad_usuario=input("digita tu ciudad: ") 
+edad_usuario=int(input("digita tu edad: ")) 
+
+CORREO_BD="prueba@gmail.com"
+CONTRASEÑA_BD="admin123"
+
+#Condicional en python
+if(correo_usuario==CORREO_BD and contraseña_usuario==CONTRASEÑA_BD):
+    print("bienvenido al sistema 🆗")
+else:
+    print("fallamos en tu autenticacion ✖️")
+
+
